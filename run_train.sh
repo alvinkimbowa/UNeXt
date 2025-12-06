104ee6cb5d16deafda69a71549ec678151dd9238
Alvin <kimbowa2alvin@gmail.com>
2025-12-05 17:45:00 -0800

    feat: update run_train.sh for enhanced training and evaluation configuration
    - Modified training and evaluation flags to allow for flexible architecture selection and dataset handling.
    - Introduced new parameters for learning rate, loss function, and input dimensions based on the selected architecture.
    - Added functionality to save predictions during evaluation.

--- run_train.sh
+++ run_train.sh
@@ -5,18 +5,31 @@ nnUNet_preprocessed="/home/ultrai/UltrAi/knee_us_segmentation/data/nnUNet_prepro
 
 export nnUNet_raw=$nnUNet_raw
 export nnUNet_preprocessed=$nnUNet_preprocessed
+export NO_ALBUMENTATIONS_UPDATE=1
 
 train=1
 eval=0
 dataset_name="Dataset073_GE_LE"
 arch="UNext"
-exp_name="Dataset073_GE_LE"
 lr=0.0001
 epochs=400
-input_w=512
-input_h=512
 b=8
-fold=0
+fold=4
+save_preds=true
+
+if [[ $arch == "UNext" || $arch == "UNext_S" ]]; then
+    min_lr=1e-5
+    loss="BCEDiceLoss"
+    input_w=512
+    input_h=512
+else
+    min_lr=1e-6
+    loss="TinyUNetLoss"
+    input_h=256
+    input_w=256
+fi
+
+echo "arch: $arch"
 
 # Evaluation settings
 test_datasets=("Dataset073_GE_LE" "Dataset072_GE_LQP9" "Dataset070_Clarius_L15" "Dataset078_KneeUS_OtherDevices")
@@ -31,7 +44,9 @@ if [[ $train -eq 1 ]]; then
         --input_w $input_w \
         --input_h $input_h \
         --b $b \
-        --fold $fold
+        --fold $fold \
+        --min_lr $min_lr \
+        --loss $loss
 fi
 
 if [[ $eval -eq 1 ]]; then
@@ -43,9 +58,11 @@ if [[ $eval -eq 1 ]]; then
             test_split="Tr"
         fi
         python val.py \
+        --name $arch \
         --train_dataset $dataset_name \
         --train_fold $fold \
         --test_dataset $test_dataset \
-        --test_split $test_split
+        --test_split $test_split \
+        --save_preds $save_preds
     done
 fi
